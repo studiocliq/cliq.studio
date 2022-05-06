9bda37627abdb6e5bb9798a21eb4f38ede9b2cfd
studiocliq <parkgone23@gmail.com>
2022-05-06 14:45:47 +0900

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `studio-cliq-tech-blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
